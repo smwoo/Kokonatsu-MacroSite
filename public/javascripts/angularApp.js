6c60f1d668db6cb53f26030b8d7c94fa7b417a9f
David Jun Cai <jdcai@uwaterloo.ca>
2016-09-24 14:26:37 -0400

    gfycat load after init

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -51,10 +51,6 @@ function($sce, $scope, macros){
     };
 
     $scope.trustAsResourceUrl = $sce.trustAsResourceUrl;
-    
-     $scope.$on('$viewContentLoaded', function(){
-        gfyCollection.init();
-    });
 }]);
 
 app.config([
@@ -76,3 +72,25 @@ function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('home');
 }]);
+
+app.directive('controller', function() {
+    return {
+        link: function($scope, element, attrs) {
+            // Trigger when number of children changes,
+            // including by directives like ng-repeat
+            var watch = $scope.$watch(function() {
+                return element.children().length;
+            }, function() {
+                // Wait for templates to render
+                $scope.$evalAsync(function() {
+                    // Finally, directives are evaluated
+                    // and templates are renderer here
+                    gfyCollection.init();
+                    var children = element.children();
+                    console.log(children);
+                    console.log("test");
+                });
+            });
+        },
+    };
+});

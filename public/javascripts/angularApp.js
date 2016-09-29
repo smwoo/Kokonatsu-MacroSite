3716a3e726108af6232b0d24cf42f22ffc64daa4
michael woo <mwoo123@gmail.com>
2016-09-28 20:35:19 -0400

    numbers before non alphanumeric char

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -39,7 +39,7 @@ function($sce, $scope, macros){
     $scope.macros = macros.macros;
     $scope.displayMacros = $scope.macros;
 
-    $scope.alphabet = ["ALL","?","#","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    $scope.alphabet = ["ALL","#","?","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     $scope.displayLetter;
 
     $scope.gfycat = function(link) {
